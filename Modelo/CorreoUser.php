c53ef4163aa19bee538865642887243751fa930b
ieraielizondo <ieraielizondo@gmail.com>
2015-11-07 01:03:51 +0100

    PHPMailer ->"OK"
    Añadido PHPMailer, probado con smtp de Google, para que funcione hay que
    activar la opción de seguridad: Permitir el acceso de aplicaciones menos
    seguras:
    Tareas-> probar otros smtp, y sino probar XOAUTH2 de Google. Login, y
    validacion del email. Visualizar mapa y añadir posiciones.

--- Modelo/CorreoUser.php
+++ Modelo/CorreoUser.php
@@ -1,7 +1,9 @@
 <?php
 
-	include 'class.phpmailer.php';
-	include 'class.smtp.php';
+	//require 'class.phpmailer.php';
+	//include 'class.smtp.php';
+	require '../vendor/phpmailer/PHPMailerAutoLoad.php';
+	date_default_timezone_set('Etc/UTC');
 
 	class CorreoUser {
 
@@ -13,24 +15,40 @@
 			$retVal=true;
 			$mail = new PHPMailer();
 			$mail->isSMTP();
-			$mail->SMTPAuth = true;
-			//$mail->SMTPSecure = "ssl";
-			$mail->Host = "smtp.live.com";
+			
+			$mail->SMTPSecure = 'tls';
+			$mail->Host = 'smtp.gmail.com';
 			$mail->Port = 587;
-			$mail->Username = "";
-			$mail->Password = "";
-			$mail->From = "";
-			$mail->FromName = "Administrador TrackingApp";
+
+			$mail->SMTPAuth = true;
+			$mail->Username = "//";
+			$mail->Password = "//";
+
+			$mail->SMTPDebug=1;
+			//$mail->Debugoutput = 'html';
+
+			$mail->addAddress($correo,$Nombre);			
+			
+			$mail->From='//';
+			$mail->FromName='Administrador TrackingApp';
+			$mail->addReplyTo('//','Administrador TrackingApp');
 			$mail->Subject = "Bienvenido a trackingApp";
 			$mail->AltBody = "Mensaje de prueba";
+			$mail->WordWrap= 50;
 			$mail->msgHTML("<h1>Bienvenido".$Nombre." ".$ape1." a trackingApp</h1><br/><br/><p>Gracias por incribirse en la app <b>App Tracking</b></p><br/>
 				<p>Su nombre de usuario: ".$idUsuario."</p>Su correo: ".$correo."<p>Ha sido inscrito correctamente, pulse en el siguiente enlace para validar:</p>");
-			$mail->addAddress($correo,$Nombre);
+			
 			$mail->isHTML(true);
+
+			//var_dump($mail->send());
 			if (!$mail->send()) {
 				echo "Error: ".$mail->ErrorInfo;
 				$retVal=false;
 				return $retVal;
+			}
+			else{
+				echo "Correo OK";
+
 			}
 			return $retVal;
 		}
