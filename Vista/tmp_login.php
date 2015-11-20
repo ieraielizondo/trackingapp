e34eed79ac86e4c644e015f69ad6d08985e02676
Ierai <ierai@Ubuntu-12-04.(none)>
2015-11-20 13:56:26 +0100

    cambio de cabecera en login

--- Vista/tmp_login.php
+++ Vista/tmp_login.php
@@ -1,8 +1,3 @@
-<?php
-	if(isset($_SESSION)){
-		echo var_dump($_SESSION);
-	}
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -34,23 +29,7 @@
 	</script>
 </head>
 <body>		
-	<nav id="main-header">
-		<a id="titulo" href="#">
-			<span class="site-name">TrackingApp</span>
-			<span class="site-desc">Web de tracking.</span>
-		</a>
-		<div id="navv" class="navbar">
-			<ul>
-				<li><audio src="./Vista/audio.mp3" preload="auto" controls autoplay></audio></li>				
-				<li><a href="#">Inicio</a></li>
-				<li><a href="#">Posiciones</a></li>
-				<li><a href="#">LogOut</a></li>
-			</ul>
-		</div>
-	</nav>
-
-
-	<main>
+		
 	<nav id="cabecera" class="navbar navbar-default">
 		<div class="container">
 			<!-- Brand and toggle get grouped for better mobile display -->
@@ -70,21 +49,13 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     	<ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
+    		<li><audio src="./Vista/audio.mp3" preload="auto" controls autoplay></audio></li>
+        	<li><a href="#">Link</a></li>			
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
+<main>
 		<section>
 			<div id="container">
 			<?php if(isset($flash['message'])):?>
