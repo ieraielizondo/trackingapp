f14ed41ce8c2da424ec3f108d6885e762345898b
Ierai <ierai@Ubuntu-12-04.(none)>
2015-11-27 13:57:36 +0100

    subida viernes

--- Vista/js/maps.js
+++ Vista/js/maps.js
@@ -1,7 +1,9 @@
-	var map=null;
+	var mapa=null;
 	var marcadres_bd=[];
 	var marcadores_nuevos=[];
+	var form=null;
 	function initMap() {
+		form=$("#formNuevo");
 		var punto=new google.maps.LatLng(43.308615, -1.893189);
 		var config={
 			zoom:16,
@@ -9,26 +11,26 @@
 			mapTypeId:google.maps.MapTypeId.ROADMAP
 		};
 
-		map = new google.maps.Map($('#mapa')[0],config);
+		mapa = new google.maps.Map($('#mapa')[0],config);
 		
 		var marcador=new google.maps.Marker({
 			position:punto,
 			title:"Mi casa",
-			map:map,
+			map:mapa,
 			animation:google.maps.Animation.DROP,
 			draggable:false
 		});
-		marcador.setMap(map);
-		google.maps.event.addListener(map,"click",function(event){
+		marcador.setMap(mapa);
+		google.maps.event.addListener(mapa,"click",function(event){
 			var coordenadas=event.latLng.toString();
-
+			var titulo=prompt("Titulo")
 			coordenadas=coordenadas.replace("(", "");
 			coordenadas=coordenadas.replace(")", "");
 			var lista=coordenadas.split(",");
-			var form=$("#formNuevo");
+			
 			form.find("input[name=lat]").val(lista[0]);
 			form.find("input[name=long]").val(lista[1]);
-
+			nuevoMarcador(lista[0],lista[1],titulo);
 			
 		});
 	}
@@ -36,17 +38,20 @@
 	function nuevoMarcador(lat,lon,titulo){
 		quitarMarcadores(marcadores_nuevos);
 		var punto=new google.maps.LatLng(lat,lon);
-		var marcardor=new google.maps.Marker({
-			position:punto;
-			title=titulo,
-			
-
+		var marcador=new google.maps.Marker({
+			position:punto,
+			title:titulo,
+			map:mapa,
+			animation:google.maps.Animation.DROP,
+			draggable:false
 		});
+		marcador.setMap(mapa);
+		marcadores_nuevos.push(marcador);
 
 	}
 	function quitarMarcadores(marcadores){
-		for(i=0,marcadores.length;i++){
-			marcadores[i].setMap=null;
+		for(i=0;marcadores.length-1;i++){
+			marcadores[i].setMap(null);
 		}
 		
 
